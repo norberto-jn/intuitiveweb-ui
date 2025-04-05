0202546d022ff95ac945064488189ac6ad51e95a
norberto-jn <norbertojunio@gmail.com>
2025-04-04 23:54:58 -0300

    fix: ajustei os ícones na listagem de operadoras

--- src/App.vue
+++ src/App.vue
@@ -104,7 +104,7 @@
         :disabled="currentPage === 1" 
         class="pagination-button"
       >
-        <i class="fas fa-chevron-left"></i>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left-icon lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
       </button>
       <span class="page-info">Page {{ currentPage }} of {{ totalPages }}</span>
       <button 
@@ -112,7 +112,7 @@
         :disabled="currentPage === totalPages" 
         class="pagination-button"
       >
-        <i class="fas fa-chevron-right"></i>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
       </button>
     </div>
 
